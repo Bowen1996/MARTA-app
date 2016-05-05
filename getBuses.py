b616752c6d483622e0dc11fc685e35539c93c51f
nikhil-kulkarni <nikhil896@gmail.com>
2016-05-05 10:33:45 -0400

    All buses

--- getBuses.py
+++ getBuses.py
@@ -34,7 +34,7 @@ def getBuses(route=''):
 def main():
 
 	# Input function to obtain route number from user
-	route = input('\n\nPlease enter a route number (leave blank for all routes):\n\n')
+	route = raw_input('\n\nPlease enter a route number (leave blank for all routes):\n\n')
 
 	# Call getBuses function with user-defined route number
 	getBuses(route)
